ccad54463917d61bc10ff4be2a3af91cbb48aa28
Miguel Beltran <m.beltrann676@gmail.com>
2023-06-05 11:53:25 -0700

    Excercises Implemented

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 user_dec = int(input("Type 1 for exception or 2 for no exception: "))
-try:
+try:  # Something that might cause an exception, probably not but just in case
     if user_dec == 1:
         y = 10 / 0
     else:
         y = 10 / 1
-except ZeroDivisionError:
+except ZeroDivisionError:  # Do this if there was an exception
     print("Error: Division by zero occured")
-else:
+else:  # Do this if there were no exceptions
     print("No exception within try block of code")
-finally:
+finally:  # Do this no matter what happens
     print("This code will run no matter if an exception was caught or not")
 
-# How am i gonna get this into git and github??? thats the real things i want to learn before i move forward
-# Get to publish all my work for this summer into github so i got something to show that I have been coding and
-# creating projects2
+print()
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"Key": "value"}
+    print(a_dictionary["kjsdfj"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+# More than 1 except can be used to catch multiple exceptions in try block of code
+except KeyError as error_message:  # Get hold of error message and use it when exception is caught!!!!
+    print(f"The key {error_message} does not exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed")
+    # raise TypeError("This is an error that I made up")
 
-# Do i start with local repo i made from Odin Project?
+print()
+# You would want to raise errors when the code is perfectly valid but will generate wrong results
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 9:
+    raise ValueError("Human height should not be over 9 feet")
+
+bmi = weight / height ** 2
+print(bmi)
+
